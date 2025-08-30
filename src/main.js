05200d9c5bc5d5e216d4e214b783e02e65cd8dc1
teklynk <teklynk@protonmail.com>
2025-08-30 19:29:22 -0400

    added prism copy to clipboard plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import 'bootstrap'
 import 'prismjs'
 import 'prismjs/components/prism-json'
 import 'prismjs/plugins/line-numbers/prism-line-numbers'
+import 'prismjs/plugins/toolbar/prism-toolbar'
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
 import moment from 'moment'
 
 window.moment = moment;
